cc2f52f81d1c48b550640dcc4bcacc86e426fb21
sch <2524356811@qq.com>
2020-04-12 14:12:40 +0800

    Initial commit

---
+++ src/main/java/com/sch/springbootkuangshen/SpringbootKuangshenApplication.java
@@ -0,0 +1,13 @@
+package com.sch.springbootkuangshen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootKuangshenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootKuangshenApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sch/springbootkuangshen/SpringbootKuangshenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sch.springbootkuangshen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootKuangshenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
